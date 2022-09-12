fe09333a05870dc31c1af7be1f088ab0de2cf887
Randy Hollines <objeck@gmail.com>
2022-09-11 22:20:54 -0700

    code clean up

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-export PATH=$PATH:~/Documents/Code/objeck-lang/core/release/deploy/bin
-export OBJECK_LIB_PATH=~/Documents/Code/objeck-lang/core/release/deploy/lib
+export PATH=$PATH:../objeck-lang/core/release/deploy/bin
+export OBJECK_LIB_PATH=../objeck-lang/core/release/deploy/lib
 
-obc -src tiny_dungeon.obs,dungeon_managers.obs,dungeon_eninities.obs -lib gen_collect,sdl2,sdl_game,misc -dest tiny_dungeon.obe
-
+obc -src tiny_dungeon.obs,dungeon_managers.obs,dungeon_eninities.obs -lib gen_collect,sdl2,json,sdl_game,misc -dest tiny_dungeon.obe
 if [ ! -z "$1" ] && [ "$1" = "brun" ]; then
 	obr tiny_dungeon.obe
 fi;
+
+
