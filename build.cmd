851f1972afa74c63232bb5447567878c0dde3272
Randy Hollines <objeck@gmail.com>
2022-09-12 11:22:37 -0700

    working on taking gold

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-obc -src tiny_dungeon.obs,dungeon_managers.obs,dungeon_eninities.obs -lib gen_collect,sdl2,json,sdl_game,misc -dest tiny_dungeon.obe -opt s0
+obc -src tiny_dungeon.obs,dungeon_managers.obs,dungeon_eninities.obs -lib gen_collect,sdl2,json,sdl_game,misc -dest tiny_dungeon.obe
 IF NOT "%~1"=="" IF "%~1"=="brun"  (
 	obr tiny_dungeon.obe
 )
